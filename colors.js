3c47eab39aa716b4b4eee24a3702a325c23e51ad
wony94 <102590344+wony94@users.noreply.github.com>
2023-03-10 15:59:29 +0900

    Add files via upload
    final

---
+++ colors.js
@@ -0,0 +1,35 @@
+var links = {
+    setColor:function(color){
+        // var alist = document.querySelectorAll('a');
+        // var i = 0;
+        // while(i < alist.length){
+        //     alist[i].style.color = color;
+        //     i = i + 1;
+        // }    
+        $('a').css('color',color);
+    }
+}
+var body = {
+    setColor:function (color){
+    // document.querySelector('body').style.color=color;
+    $('body').css('color',color);
+    },
+    setBackgroundColor:function (color){
+    // document.querySelector('body').style.backgroundColor=color;
+    $('body').css('backgroundColor',color);
+    }
+}
+function nightDayHandler(self){
+    var target = document.querySelector('body');
+    if(self.value === 'night'){
+        body.setBackgroundColor('black');
+        body.setColor('white');
+        self.value='day';
+        links.setColor('powderblue'); 
+    } else {
+        body.setBackgroundColor('white');
+        body.setColor('black');
+        self.value='night';
+        links.setColor('blue');
+        }
+}        
